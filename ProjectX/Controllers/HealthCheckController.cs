cb99b3e584512a31e4bc18ffa4af9b7d419ef412
eriskcn <hoaikcn7@gmail.com>
2025-03-25 01:27:21 +0700

    mod: health-check

---
+++ ProjectX/Controllers/HealthCheckController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectX.Controllers;
+
+[ApiController]
+[Route("capablanca/api/v0/health-check")]
+public class HealthCheckController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new
+        {
+            Message = "Server is warm",
+            Bingo = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        });
+    }
+
+    [HttpGet("secret")]
+    [Authorize]
+    public IActionResult GetSecret()
+    {
+        return Ok(new
+        {
+            Message = "Authorized",
+            Bingo = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        });
+    }
+}
