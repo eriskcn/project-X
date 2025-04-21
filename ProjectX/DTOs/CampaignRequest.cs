3b0fc4e5be03456a5d1f8831aa461215aa8185cf
eriskcn <hoaikcn7@gmail.com>
2025-04-21 14:39:23 +0700

    mod: default status for create campaign

--- ProjectX/DTOs/CampaignRequest.cs
+++ ProjectX/DTOs/CampaignRequest.cs
@@ -5,20 +5,13 @@ namespace ProjectX.DTOs;
 
 public class CampaignRequest
 {
-    [Required]
-    [StringLength(100)]
-    public required string Name { get; set; }
+    [Required] [StringLength(100)] public required string Name { get; set; }
 
-    [Required]
-    [StringLength(10000)]
-    public required string Description { get; set; }
+    [Required] [StringLength(10000)] public required string Description { get; set; }
 
-    [Required]
-    public required DateTime Open { get; set; }
+    [Required] public required DateTime Open { get; set; }
 
-    [Required]
-    public required DateTime Close { get; set; }
+    [Required] public required DateTime Close { get; set; }
 
-    [Required]
-    public required CampaignStatus Status { get; set; }
+    [Required] public required CampaignStatus Status { get; set; } = CampaignStatus.Opened;
 }
