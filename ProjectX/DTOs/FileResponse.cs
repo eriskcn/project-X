cb86db82ab7c030632eca3588f0a21a108d23f7f
eriskcn <hoaikcn7@gmail.com>
2025-04-22 23:41:41 +0700

    mod: add TargetId to FileResponse

--- ProjectX/DTOs/FileResponse.cs
+++ ProjectX/DTOs/FileResponse.cs
@@ -5,6 +5,7 @@ namespace ProjectX.DTOs;
 public class FileResponse
 {
     public Guid Id { get; set; }
+    public Guid TargetId { get; set; }
     public required string Name { get; set; }
     public required string Path { get; set; }
     public DateTime Uploaded { get; set; }
