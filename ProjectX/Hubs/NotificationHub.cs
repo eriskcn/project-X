b615b14009321c78356cf6876f9e7fe99f878128
eriskcn <hoaikcn7@gmail.com>
2025-04-25 16:49:12 +0700

    fix: remove connection mapping

--- ProjectX/Hubs/NotificationHub.cs
+++ ProjectX/Hubs/NotificationHub.cs
@@ -2,27 +2,6 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace ProjectX.Hubs;
 
-public class NotificationHub(ConnectionMapping<Guid> connectionMapping) : Hub
+public class NotificationHub : Hub
 {
-    public override Task OnConnectedAsync()
-    {
-        var userId = Context.UserIdentifier;
-        if (Guid.TryParse(userId, out var parsedId))
-        {
-            connectionMapping.Add(parsedId, Context.ConnectionId);
-        }
-
-        return base.OnConnectedAsync();
-    }
-
-    public override Task OnDisconnectedAsync(Exception? exception)
-    {
-        var userId = Context.UserIdentifier;
-        if (Guid.TryParse(userId, out var parsedId))
-        {
-            connectionMapping.Remove(parsedId, Context.ConnectionId);
-        }
-
-        return base.OnDisconnectedAsync(exception);
-    }
 }
